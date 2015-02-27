739261899fdd150555c79e58ad6b1ca10215354c
Selech <selech@live.dk>
2015-02-27 18:00:57 +0100

    Vigtig commit!
    Jeg har sat det til “force text” som assets. Dette sikre at vores git
    fungere optimalt.
    Har desuden opdateret .gitignore, men det virker ikke rigtig synes
    jeg?..

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -23,12 +23,14 @@ public class PlayerScript : MonoBehaviour {
 			
 			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
 			Vector3 targetposition = new Vector3 (target.transform.position.x, transform.position.y, target.transform.position.z);
-			transform.position = Vector3.MoveTowards (transform.position, targetposition, 0.008f);
+			transform.position = Vector3.MoveTowards (transform.position, targetposition, 0.015f);
 			transform.LookAt (targetposition);
 
 			if (transform.position == targetposition) {
 				animator.SetTrigger ("TriggerIdle");
 				animator.ResetTrigger ("TriggerWalk");
+
+
 
 				target = null;
 			}
