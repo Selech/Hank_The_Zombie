6a3cfec8e03466c371bf02f91def3f4aee332bdb
juriw <juriwulffonline@gmail.com>
2015-03-25 12:14:58 +0100

    wallSwitch, and a new test track
    Test track still under development.

--- Assets/Scripts/enviroment_controllers/WallSwitchScript.cs
+++ Assets/Scripts/enviroment_controllers/WallSwitchScript.cs
@@ -7,19 +7,25 @@ public class WallSwitchScript : MonoBehaviour
 	public GameObject switchObject;
 	private float nonActiveZ;
 	public float activeZ;
-
+	public int isSet = 0;
 	void Start ()
 	{
 		nonActiveZ = this.transform.localScale.z;
 	}
 
-	void OnCollisionExit(){
-		switchObject.SetActive(true);
+	void OnCollisionExit ()
+	{
+		isSet--;
+		if (isSet == 0) {
+			switchObject.SetActive (true);
+		}
+
 	}
 
 
 	void OnCollisionEnter ()
 	{
+		isSet++;
 		Vector3 scale = new Vector3 (this.transform.localScale.x, this.transform.localScale.y, activeZ);
 		this.transform.localScale = scale;
 		switchObject.SetActive (false);
