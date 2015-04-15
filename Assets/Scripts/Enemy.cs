00767b977eadc85ef7552286dbe5cfb69d9d07f1
juriw <juriwulffonline@gmail.com>
2015-04-15 13:24:24 +0200

    Enemy and arms

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	private MouseControls GameController;
+
+	// Use this for initialization
+	void Start () {
+		this.GameController = GameObject.Find ("Controller").GetComponent<MouseControls>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 TargetPlayer = new Vector3(this.GameController.player.transform.position.x, transform.position.y, this.GameController.player.transform.position.z);
+		transform.position = Vector3.MoveTowards (transform.position, TargetPlayer, 0.008f);
+		transform.LookAt (TargetPlayer);
+	}
+}
+ 
