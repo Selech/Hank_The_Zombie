0e44ffbde01723eed41c825eba4a8a6c91c09e99
Anders Bruun Rosbæk <rosbaekmail@gmail.com>
2015-04-18 23:53:17 +0200

    Zoom starts at 50 can zoom in to 25 and zoom out to 100. Camera movement fixed (Some touch complications compared to mouse use).

--- Assets/Scripts/TheEditor/MouseRts.cs
+++ Assets/Scripts/TheEditor/MouseRts.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MouseRts : MonoBehaviour
 {
@@ -7,51 +8,90 @@ public class MouseRts : MonoBehaviour
 	
 	private const int ScrollArea = 25;
 	private const int ScrollSpeed = 7;
-	private const int DragSpeed = 7;
-	
-	private const int ZoomSpeed = 25;
+	private const float DragSpeed = 1.2f;
+	private float xDiff = 0;
+	private float yDiff = 0;
+
+	private bool readyForMoving = false;
+	private Vector2[] touchPoints = new Vector2[2];
+	private int indexPointer = 0;
+	private float deltaMagnitudeDiff;
+	private const int ZoomSpeed = 10;
 	private const int ZoomMin = 25;
-	private const int ZoomMax = 100;
+	private const int ZoomMax = 140;
+	private bool Zooming;
+	private int ZoomCur = 50;
 	
 	private const int PanSpeed = 7;
 	private const int PanAngleMin = 50;
 	private const int PanAngleMax = 80;
 
 	// Zooming part
-	public float perspectiveZoomSpeed = 0.5f;        // The rate of change of the field of view in perspective mode.
-	public float orthoZoomSpeed = 0.5f;        // The rate of change of the orthographic size in orthographic mode.
+	private const float perspectiveZoomSpeed = 0.5f;        // The rate of change of the field of view in perspective mode.
+	private const float orthoZoomSpeed = 0.5f;        // The rate of change of the orthographic size in orthographic mode.
+
+	public Text txt;
+
+	private Transform cameraTransform;
 
 	// Set Orientation
 	void Start()
 	{
+		cameraTransform = Camera.main.transform;
 		Screen.orientation = ScreenOrientation.LandscapeLeft;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if(Popup.PopupOpen == false)
+		if (Popup.PopupOpen == false)
 		{
-			// Check for Zoom
-			CheckforZoom();
+			if (Input.touchCount == 2)
+			{
+				// Calculate delta in touch/mouse-positions 
+				CalcDeltaTouch();
+				
+				// Check for Zoom
+				CheckForZoom();
+				
+				// Init camera translation for this frame.
+				CheckForMoving();
+			}
+			else
+			{
+				Zooming = false;
+			}
+		}
+	}
 
-			// Init camera translation for this frame.
+	void CalcDeltaTouch()
+	{
+		// Store both touches.
+		Touch touchZero = Input.GetTouch(0);
+		Touch touchOne = Input.GetTouch(1);
+		
+		// Find the position in the previous frame of each touch.
+		Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+		Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+		// Find the magnitude of the vector (the distance) between the touches in each frame.
+		float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+		float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+		
+		// Find the difference in the distances between each frame.
+		deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+		// Update touch-Points
+		touchPoints[indexPointer] = touchZeroPrevPos;
+		indexPointer++;
+		if(indexPointer > 1){ indexPointer = 0; readyForMoving = true; }
+	}
+
+	void CheckForMoving()
+	{
+		if (Zooming == false && readyForMoving)
+		{
 			var translation = Vector3.zero;
-			
-//			// Zoom in or out
-//			var zoomDelta = Input.GetAxis("Mouse ScrollWheel")*ZoomSpeed*Time.deltaTime;
-//			if (zoomDelta!=0)
-//			{
-//				translation -= Vector3.up * ZoomSpeed * zoomDelta;
-//			}
-//			
-//			// Start panning camera if zooming in close to the ground or if just zooming out.
-//			var pan = GetComponent<Camera>().transform.eulerAngles.x - zoomDelta * PanSpeed;
-//			pan = Mathf.Clamp(pan, PanAngleMin, PanAngleMax);
-//			if (zoomDelta < 0 || GetComponent<Camera>().transform.position.y < (ZoomMax / 2))
-//			{
-//				GetComponent<Camera>().transform.eulerAngles = new Vector3(pan, 0, 0);
-//			}
 			
 			// Move camera with arrow keys
 			translation += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
@@ -59,68 +99,51 @@ public class MouseRts : MonoBehaviour
 			// Move camera with mouse
 			if (Input.GetMouseButton(1)) // MMB
 			{
+				xDiff = Mathf.Abs(touchPoints[0].x) - Mathf.Abs(touchPoints[1].x);
+				yDiff = Mathf.Abs(touchPoints[0].y) - Mathf.Abs(touchPoints[1].y);
+
 				// Hold button and drag camera around
-				translation -= new Vector3(Input.GetAxis("Mouse X") * DragSpeed * Time.deltaTime, 0,
-				                           Input.GetAxis("Mouse Y") * DragSpeed * Time.deltaTime);
-			}
-			
-			// Keep camera within level and zoom area
-			var desiredPosition = GetComponent<Camera>().transform.position + translation;
-			if (desiredPosition.x < -LevelArea || LevelArea < desiredPosition.x)
-			{
-				translation.x = 0;
-			}
-			if (desiredPosition.y < ZoomMin || ZoomMax < desiredPosition.y)
-			{
-				translation.y = 0;
-			}
-			if (desiredPosition.z < -LevelArea || LevelArea < desiredPosition.z)
-			{
-				translation.z = 0;
+				if (indexPointer==0)
+				{
+					if(Mathf.Abs(xDiff)<50 && Mathf.Abs(yDiff)<50)
+					{
+						translation -= new Vector3(-(xDiff * DragSpeed * Time.deltaTime), 0,
+						                           -(yDiff * DragSpeed * Time.deltaTime));
+					}
+				}
 			}
 			
 			// Finally move camera parallel to world axis
 			GetComponent<Camera>().transform.position += translation;
 		}
 	}
-
-	void CheckforZoom()
+	
+	void CheckForZoom()
 	{
-		// If there are two touches on the device...
-		if (Input.touchCount == 2)
+		string pinchZoomingOut = "";
+
+		if (Mathf.Abs(deltaMagnitudeDiff)>3)
 		{
-			// Store both touches.
-			Touch touchZero = Input.GetTouch(0);
-			Touch touchOne = Input.GetTouch(1);
-			
-			// Find the position in the previous frame of each touch.
-			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
-			
-			// Find the magnitude of the vector (the distance) between the touches in each frame.
-			float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
-			
-			// Find the difference in the distances between each frame.
-			float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
-			
-			// If the camera is orthographic...
-			if (GetComponent<Camera>().orthographic)
-			{
-				// ... change the orthographic size based on the change in distance between the touches.
-				GetComponent<Camera>().orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
-				
-				// Make sure the orthographic size never drops below zero.
-				GetComponent<Camera>().orthographicSize = Mathf.Max(GetComponent<Camera>().orthographicSize, 0.1f);
+			Zooming = true;
+			pinchZoomingOut = (deltaMagnitudeDiff > 0) ? "zoomOut" : "zoomIn";
+
+			// Zooming in
+			if ((Input.GetKey("t") || Zooming) && ZoomCur>ZoomMin && pinchZoomingOut=="zoomIn") {
+				ZoomCur -= ZoomSpeed;
+				transform.position += cameraTransform .forward * (Time.deltaTime * ZoomSpeed);//deltaMagnitudeDiff);
+				Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 60, 2*Time.deltaTime);
 			}
-			else
+			// Zooming out
+			else if((Input.GetKey("g") || Zooming) && ZoomCur<ZoomMax && pinchZoomingOut=="zoomOut")
 			{
-				// Otherwise change the field of view based on the change in distance between the touches.
-				GetComponent<Camera>().fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
-				
-				// Clamp the field of view to make sure it's between 0 and 180.
-				GetComponent<Camera>().fieldOfView = Mathf.Clamp(GetComponent<Camera>().fieldOfView, 0.1f, 179.9f);
+				ZoomCur += ZoomSpeed;
+				transform.position -= cameraTransform .forward * (Time.deltaTime * ZoomSpeed);//deltaMagnitudeDiff);
+				Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 60, 2*Time.deltaTime);
 			}
+		}
+		else if (Zooming && Mathf.Abs(deltaMagnitudeDiff)<0.5)
+		{
+			Zooming = false;
 		}
 	}
 }
