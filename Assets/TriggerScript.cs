5c96078880a56d9cf6533947297a8c8200fc0eea
Selech <selech@live.dk>
2015-04-23 09:57:48 +0200

    Første tutorial bane!

---
+++ Assets/TriggerScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerScript : MonoBehaviour {
+
+	public GameObject canvas;
+
+	void OnTriggerEnter(){
+		canvas.SetActive (true);
+	}
+
+}
